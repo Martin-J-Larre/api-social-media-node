ea9ab844142f035c81674d724e4568ce2100e1e0
Martin-J-Larre <martinjlarre@gmail.com>
2023-02-03 01:10:04 -0500

    #6 User controller login password validate with bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -30,7 +30,7 @@ const register = (req, res) => {
         message: 'Nickname or email already exist'
       });  
     }
-    // bcrypt password
+
     const hashedPassword = await bcrypt.hash(data.password, 10);
     data.password = hashedPassword;
 
@@ -52,6 +52,54 @@ const register = (req, res) => {
   });
 }
 
+const login = (req, res) => {
+
+  const data = req.body;
+
+  if (!data.email || !data.password) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'You should enter email and password'
+    });
+  }
+
+  User.findOne({ email: data.email })
+      .exec((error, user) => {
+
+      if (error || !user) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'User does not exist'
+        });
+      }
+
+    // compare bcrypt
+    const userPassword = bcrypt.compareSync(data.password, user.password);
+    if (!userPassword) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Password incorrect'
+      });
+    }
+
+    // jwt
+    const token = false;
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Email and password correct',
+      user: {
+        id: user.id,
+        name: user.name,
+        nickname: user.nickname
+      },
+      token
+    });
+  });
+}
+
+
 module.exports = {
-  register
+  register,
+  login
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { register } = require('../controllers/user');
+const { register, login } = require('../controllers/user');
 
-router.post('/register', register)
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
